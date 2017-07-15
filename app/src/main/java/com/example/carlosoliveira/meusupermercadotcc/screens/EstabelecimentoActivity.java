663413a59b9a4cef3c2326a11fcbe5557be2c613
Carlos Oliveira <vinicius.study@gmail.com>
2017-07-14 21:38:28 -0300

    Commit inicial do projeto, com o inicio da estrutura e classes MainActivy e Estabelecimento

---
+++ app/src/main/java/com/example/carlosoliveira/meusupermercadotcc/screens/EstabelecimentoActivity.java
@@ -0,0 +1,17 @@
+package com.example.carlosoliveira.meusupermercadotcc.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.carlosoliveira.meusupermercadotcc.R;
+
+public class EstabelecimentoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estabelecimento);
+
+       // setTitle(getResources().getString(R.string.title_activity_estabelecimento));
+    }
+}
---
+++ app/src/main/java/com/example/carlosoliveira/meusupermercadotcc/screens/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.carlosoliveira.meusupermercadotcc.screens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.carlosoliveira.meusupermercadotcc.R;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Drawer
+    private Drawer result = null;
+
+    //Toolbar
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        //Inicio AccountHeader
+        //####################### SÓ O CABEÇALHO #######################
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.meusuper1)
+                .addProfiles(
+                        //new ProfileDrawerItem().withName("Thiago Cury").withEmail("thiagocury@gmail.com").withIcon(getResources().getDrawable(R.mipmap.ic_launcher))
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener(){
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .build();
+
+        //Menu
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withTranslucentStatusBar(false)
+                .withToolbar(toolbar)
+                .withAccountHeader(headerResult)
+                .withSavedInstance(savedInstanceState)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Cliente").withIdentifier(0).withIcon(R.mipmap.ic_launcher),
+                        new SecondaryDrawerItem().withName("Estabelecimento").withIdentifier(1).withIcon(R.mipmap.ic_launcher)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+
+                        switch ((int)drawerItem.getIdentifier()){
+                            case 0:
+                                Toast.makeText(getBaseContext(),"Você clicou no menu Cliente",Toast.LENGTH_LONG).show();
+                                break;
+
+                            case 1:
+                                //Toast.makeText(getBaseContext(),"Você clicou no menu Estabelecimento",Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(MainActivity.this, EstabelecimentoActivity.class);
+                                startActivity(intent);
+                                break;
+                        }
+                        return false;
+                    }
+                }).build();
+    }
+
+}
